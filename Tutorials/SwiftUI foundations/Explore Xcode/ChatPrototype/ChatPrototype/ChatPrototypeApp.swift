226ef057cc4163e566a37918fc76b17c33e55758
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-04 04:41:49 +0000

    setup ChatPrototype

---
+++ Tutorials/SwiftUI foundations/Explore Xcode/ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Jeevan Chandra Joshi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
