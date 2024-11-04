9ecc9d69c6f80efb8e242daba7392b471dcffdae
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-04 06:13:48 +0000

    setup OnboardingFlow

---
+++ Tutorials/SwiftUI foundations/Layout and style/OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Jeevan Chandra Joshi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
