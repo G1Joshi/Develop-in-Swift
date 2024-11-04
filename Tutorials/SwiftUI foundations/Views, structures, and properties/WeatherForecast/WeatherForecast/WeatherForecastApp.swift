6a41cd0b92dbce9726f42d8d0e466c1ed6a153c2
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-04 05:21:24 +0000

    setup WeatherForecast

---
+++ Tutorials/SwiftUI foundations/Views, structures, and properties/WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Jeevan Chandra Joshi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
