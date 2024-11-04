df190aa99b98183faaae0df13b742a93792015a5
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-04 15:26:48 +0000

    setup Pick-a-Pal

---
+++ Tutorials/SwiftUI foundations/Lists and text fields/Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Jeevan Chandra Joshi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
