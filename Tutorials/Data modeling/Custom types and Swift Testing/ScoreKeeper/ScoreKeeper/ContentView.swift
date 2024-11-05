70a8fffc3d30a5db34a1defffaa6c84be6820832
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-05 07:41:04 +0000

    add score keeper

--- Tutorials/Data modeling/Custom types and Swift Testing/ScoreKeeper/ScoreKeeper/ContentView.swift
+++ Tutorials/Data modeling/Custom types and Swift Testing/ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [Player] = [
+        Player(name: "", score: 0),
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Score Keeper")
+                .font(.title)
+                .bold()
+                .padding(.bottom)
+
+            Grid {
+                GridRow {
+                    Text("Player")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                }
+                .font(.headline)
+
+                ForEach($players) { $player in
+                    GridRow {
+                        TextField("Name", text: $player.name)
+                        Text("\(player.score)")
+                        Stepper("", value: $player.score)
+                            .labelsHidden()
+                    }
+                }
+            }
+            .padding(.vertical)
+
+            Button("Add Player ", systemImage: "plus") {
+                players.append(Player(name: "", score: 0))
+            }
+
+            Spacer()
         }
         .padding()
     }
