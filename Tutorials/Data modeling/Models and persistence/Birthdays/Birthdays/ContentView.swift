cdc506798b1a03ce457139bdfa89373044706f2d
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-05 13:33:58 +0000

    add friends

--- Tutorials/Data modeling/Models and persistence/Birthdays/Birthdays/ContentView.swift
+++ Tutorials/Data modeling/Models and persistence/Birthdays/Birthdays/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = []
+
+    @State private var newName: String = ""
+    @State private var newDate: Date = .now
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+
+                    Spacer()
+
+                    Text(friend.birthday, format: .dateTime.day().month().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+
+                    DatePicker(selection: $newDate, in: Date.distantPast ... Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+
+                        newName = ""
+                        newDate = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
---
+++ Tutorials/Data modeling/Models and persistence/Birthdays/Birthdays/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Jeevan Chandra Joshi on 05/11/24.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let birthday: Date
+}
