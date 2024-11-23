e78bf0a1eced36847fce3dae98020a94c3c76ae7
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-23 14:13:36 +0000

    setup FriendsFavoriteMovies

---
+++ Tutorials/Data modeling/Navigation, editing, and relationships/FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Jeevan Chandra Joshi on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
