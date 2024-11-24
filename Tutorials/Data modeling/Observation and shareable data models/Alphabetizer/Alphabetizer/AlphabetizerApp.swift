8722bbfa3292e6d0b9dfc1a3fd621aae1a2f072b
Jeevan Joshi <joshijeevan98@gmail.com>
2024-11-24 05:33:34 +0000

    setup Alphabetizer

---
+++ Tutorials/Data modeling/Observation and shareable data models/Alphabetizer/Alphabetizer/AlphabetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphabetizerApp.swift
+//  Alphabetizer
+//
+//  Created by Jeevan Chandra Joshi on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AlphabetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
